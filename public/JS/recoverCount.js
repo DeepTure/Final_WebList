6f0b9172ce53a7d8918c8890766afa06a8b3a79d
jafet5757 <moranorozcokevinjafet@gmail.com>
2021-05-06 22:24:38 -0500

    prepare enviroment variables

---
+++ public/JS/recoverCount.js
@@ -0,0 +1,7 @@
+//acciones de inicio
+$('#sendEmail').click(function(){
+    const email = document.getElementById('email').value;
+    //ahora debemos revisar que exista el correo y levantar el token
+});
+
+$('.second-action').hide();
