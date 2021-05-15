dbf2d1ec892a81af6ad843b6cf2e25b770a7e989
jafet5757 <moranorozcokevinjafet@gmail.com>
2021-05-15 16:51:30 -0500

    already generate and save list tokens

--- models/homeTeacher.js
+++ models/homeTeacher.js
@@ -1,4 +1,5 @@
 const model = {};
+const jwt = require("jsonwebtoken");
 const db = require("../database/connection");
 
 //problemas con el modo asincrono SELECT id_grupo FROM egeneracion WHERE id_generacion=?
@@ -16,12 +17,26 @@ model.getGroups = (req, res)=>{
     });
 };
 
-//esta funcion ya queda obsoleta
-model.getSubjects = (req,res)=>{
+model.addToken = (req, res)=>{
     const data = req.body;
-    db.query('SELECT * FROM mprograma WHERE id_empleado = ?',[data.id], (err, programas)=>{
-        if(err) return res.json(err);
-        return res.send(programas);
+    const time = new Date();
+    const code = generateCode(6);
+    const token = jwt.sign({ idPrograma:data.program}, code);
+    db.query("INSERT INTO etokenlista VALUES(?,?,?,?)",[token, processDuration(data.duration), time, data.program], (err, response)=>{
+        if(err)return res.json(err)
+        return res.json({response,code});
+    });
+};
+
+model.verifyToken = (req, res)=>{
+    const data = req.body;
+    db.query('SELECT id_programa FROM mprograma WHERE id_empleado = ?', [data.id], (err, programas)=>{
+        if(err)return res.json(err)
+        const querys = getStringQueryTokensByPrograms(programas);
+        db.query(querys, (err, coincidencias)=>{
+            if(err)return res.json(err);
+            res.send(coincidencias);
+        });
     });
 };
 
@@ -29,10 +44,32 @@ model.getSubjects = (req,res)=>{
 function getSyringGroupsByPrograms(programas){
     let querys = '';
     programas.forEach((programa)=>{
-        let aux = 'SELECT id_grupo FROM egeneracion WHERE id_generacion="'+programa.id_generacion+'";';
+        let aux = 'SELECT id_grupo, id_generacion FROM egeneracion WHERE id_generacion="'+programa.id_generacion+'";';
         querys += aux;
     });
     return querys;
+}
+
+function getStringQueryTokensByPrograms(programas){
+    let querys = '';
+    programas.forEach((programa)=>{
+        let aux = 'SELECT id_token, duracion FROM etokenlista WHERE id_programa = "'+programa.id_programa+'";';
+        querys += aux;
+    });
+    return querys;
+}
+
+function generateCode(n) {
+    let code = "";
+    for (var i = 0; i <= n; i++) {
+        code += Math.floor(Math.random() * (9 - 0)) + 0;
+    }
+    return code;
+}
+
+function processDuration(duration){
+    let aux = duration.split(' ');
+    return aux[0];
 }
 
 module.exports = model;
--- routes/homeTeacherRoutes.js
+++ routes/homeTeacherRoutes.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const model = require('../models/homeTeacher.js');
 
 router.post('/home/getGroups',model.getGroups);
-router.post('/home/getSubjects',model.getSubjects);
+router.post('/home/addToken',model.addToken);
+router.post('/home/verifyToken', model.verifyToken);
 
 module.exports = router;
