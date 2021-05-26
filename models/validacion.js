42e10e6a99004701015b1e4f3e62f58443cf9c46
LeoYeudiel <lyeuprsw@gmail.com>
2021-05-26 10:56:18 -0500

    basic validation login

--- models/validacion.js
+++ models/validacion.js
@@ -1,10 +1,20 @@
-var patron_nombre = /^[áéíóúÁÉÍÓÚña-zA-Z\s]{1,50}/;
-var patron_ApellidoPM = /^[áéíóúÁÉÍÓÚña-zA-Z]{1,20}/;
-var patron_numEmpleado = /^[0-9]{10}/;
-var patron_boleta = /^[0-9]{10}/;
+var patron_idrol = /^[0-9]{1,10}$/;
+var patron_nombre = /^[áéíóúÁÉÍÓÚña-zA-Z\s]{1,50}$/;
+var patron_ApellidoPM = /^[áéíóúÁÉÍÓÚña-zA-Z]{1,20}$/;
+var patron_numEmpleado = /^[0-9]{10}$/;
+var patron_boleta = /^[0-9]{10}$/;
 var patron_email =
     /^(^[\w+.]+@{1}[a-z]+(([.](com|web|org|gob|ipn)){1}([.](jp|es|mx))?){1}$){1}/;
 var patron_contra = /^[0-9a-zA-Z$@$!%*?&]{8,100}$/;
+
+function idrol(id) {
+    if (patron_idrol.test(id)) {
+        return true;
+    } else {
+        console.log("Validacion del id no aprobada");
+        return false;
+    }
+}
 
 function Nombres(nombre) {
     if (patron_nombre.test(nombre)) {
@@ -61,6 +71,7 @@ function contra(pass) {
 }
 
 module.exports = {
+    idrol,
     Nombres,
     ApellidoPM,
     numEmpleado,
