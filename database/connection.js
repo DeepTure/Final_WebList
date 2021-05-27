3a1062f3c486c4daedf8fb242034b4a60a507f60
Jafet5757 <moranorozcokevinjafet@gmail.com>
2021-05-26 23:21:28 -0500

    new timer

--- database/connection.js
+++ database/connection.js
@@ -3,8 +3,8 @@ const mysql = require("mysql");
 //se le da paramentros a la conexion
 const mysqlConnection = mysql.createConnection({
     host: "localhost",
-    user: "root",
-    password: "n0m3l0",
+    user: "root3",
+    password: "root3",
     database: "weblistv2",
     multipleStatements: true,
 });
--- public/JS/timer.js
+++ public/JS/timer.js
@@ -30,8 +30,38 @@ function fecha(){
     }
 }
 
-setInterval(function(){
+/*setInterval(function(){
                 fecha();
-            },1000)
+            },1000)*/
 
 updateTime('0');
+
+//En esta parte comienza el timer
+var tiempoFaltante = new Date();
+
+/**
+ * Hace funcionar el reloj
+ * @param {Date} missingTime El tiempo que falta para que termine en minutos y segundos
+ * @returns {Boolean} si el reloj ya terminó
+ */
+function timer(missingTime){
+    if(missingTime){tiempoFaltante = missingTime}
+    tiempoFaltante.setSeconds(tiempoFaltante.getSeconds() -1)
+    $("#minuto").text(tiempoFaltante.getMinutes())
+    $("#segundo").text(tiempoFaltante.getSeconds())
+    
+    //si el tiempo es cero es porque ya termino
+    if((tiempoFaltante.getMinutes() == 0) && (tiempoFaltante.getSeconds() == 0)){
+        console.log("Ya chingastes");
+        return true;
+    }else{
+        return false;
+    }
+}
+
+let secondAfterSecond = setInterval(function(){
+    let end = timer();
+    if(end){
+        clearInterval(secondAfterSecond);
+    }
+},1000);
