623026fd02853143b0054bbb1d97f559f06d1c39
LeoYeudiel <lyeuprsw@gmail.com>
2021-06-01 11:56:43 -0500

    Chatbot

--- database/connection.js
+++ database/connection.js
@@ -3,8 +3,8 @@ const mysql = require("mysql");
 //se le da paramentros a la conexion
 const mysqlConnection = mysql.createConnection({
     host: "localhost",
-    user: "ass",
-    password: "ass",
+    user: "root",
+    password: "n0m3l0",
     database: "weblistv2",
     multipleStatements: true,
 });
