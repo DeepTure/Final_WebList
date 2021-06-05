f6bd1562a305c18cb13f4b4d3e0516119fd08326
Jafet5757 <moranorozcokevinjafet@gmail.com>
2021-06-04 19:59:08 -0500

    resports routes

--- models/sendEmailReports.js
+++ models/sendEmailReports.js
@@ -1,5 +1,6 @@
 const actions = {};
 const nodemailer = require("nodemailer");
+const db = require("../database/connection");
 
 //funciones de configuracion
 const transporter = nodemailer.createTransport({
@@ -21,17 +22,26 @@ function sendGeneralError(title, message) {
             "Buen día, se ha registrado el problema siguiente:" + message,
     };
 
-    transporter.sendMail(mailOptions, (err, info) => {
-        try {
-            if (err) {
-                console.log("Error: " + err);
-            } else {
-                console.log("Email enviado");
-            }
-        } catch (error) {
-            console.log("Error inesperado al enviar correo: " + error);
-            console.log(error.message);
-        }
+    db.query('SELECT id_usuario from EAdministrador', (err, ida)=>{
+        if(err)console.log(err);
+        const querys = getEmailAdmins(ida);
+        db.query(querys, (err, emails)=>{
+            if(err)console.log(err);
+            emails.forEach((email)=>{
+                transporter.sendMail(mailOptions, (err, info) => {
+                    try {
+                        if (err) {
+                            console.log("Error: " + err);
+                        } else {
+                            console.log("Email enviado");
+                        }
+                    } catch (error) {
+                        console.log("Error inesperado al enviar correo: " + error);
+                        console.log(error.message);
+                    }
+                });
+            });
+        });
     });
 }
 
@@ -42,6 +52,14 @@ Descripción de la falla.
 Evidencias de respaldo (imágenes, videos, etc).*/
 actions.generalReport = (title, message)=>{
     sendGeneralError(title, message);
+}
+
+function getEmailAdmins(ids){
+    querys = '';
+    ids.forEach((id)=>{
+        querys += 'SELECT email FROM CUsuario WHERE id_usuario = "'+id.id_usuario+'";';
+    });
+    return querys;
 }
 
 module.exports = actions;
